b01c64e49baa7634ab58395427adf0ec9df3a5f6
AmauryBerger <amaury.berger.pro@gmail.com>
2020-11-10 16:43:05 +0000

    ‘commit’

---
+++ amaury.js
@@ -0,0 +1,51 @@
+// Question N°3 & 4
+var planets = [ 'mercury', 'venus', 'earth', 'mars', 'jupiter'];
+console.log(planets.indexOf('mars'));
+
+// Question N°5
+var a = 5, b = 'Hello', c = 'World!', d = 10;
+a + b;
+console.log(a + ' ' + b);
+b + c;
+console.log(b + ' ' + c);
+a + d;
+console.log(a + d);
+
+// Question N°6
+var a = 5, b = 10, c = "5";
+var x = a;
+a == c;
+console.log(a == c);
+a === c;
+console.log(a === c);
+a == x;
+console.log(a == x);
+a != b;
+console.log(a != b);
+a > b;
+console.log(a > b);
+a < b;
+console.log(a < b);
+a >= b;
+console.log(a >= b);
+a <= b;
+console.log(a <= b);
+a >= c;
+console.log(a >= c);
+a <= c;
+console.log(a <= c);
+
+// Question 7 & 8
+
+var Hello = function() {
+    return 'Hello World';
+};
+var answer = Hello();
+console.log(answer);
+
+// Question 9
+var hello = function() {
+    return 'Hi!';
+};
+var x = hello();
+console.log(x);
